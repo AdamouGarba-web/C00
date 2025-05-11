a77dfb9f477f4158717acde4d230f04914e697e5
AdamouGarba-web <adamougarbar@gmail.com>
2025-05-11 23:32:10 +0100

    Add files via upload

---
+++ check_file_exist.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Script pour vérifier si un fichier donné existe
+
+# Demander le nom du fichier à l'utilisateur
+read -p "Entrez le nom du fichier : " filename
+
+# Vérifie si le fichier existe
+if [ -f "$filename" ]; then
+    echo "Le fichier '$filename' existe."
+else
+    echo "Le fichier '$filename' n'existe pas."
+fi
