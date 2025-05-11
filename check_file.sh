6ab5a76269bcc0e1575e2d1e8fa690963310d29f
AdamouGarba-web <adamougarbar@gmail.com>
2025-05-11 22:17:51 +0100

    Delete check_file.sh

--- check_file.sh
+++
@@ -1,9 +0,0 @@
-#!/data/data/com.termux/files/usr/bin/bash
-
-read -p "Entrez le nom du fichier : " filename
-
-if [ -f "$filename" ]; then
-    echo "Le fichier '$filename' existe"
-else
-    echo "Le fichier '$filename' n'existe pas"
-fi
